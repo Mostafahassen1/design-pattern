1b1800346df3c1d0f746c0b3c92a79eee5d38da7
Mostafahassen1 <134046265+Mostafahassen1@users.noreply.github.com>
2024-02-01 07:38:47 +0200

    Create Main.java

--- Singleton/before_singleton/Main.java
+++ Singleton/before_singleton/Main.java
@@ -4,7 +4,8 @@ public class Main {
         Counter obj1 = new Counter();
         Counter obj2 = new Counter() ;
 
-        obj1.addCount();
-        obj2.addCount();
+        obj1.addCount(); // This object will print 1 
+        obj2.addCount(); // This object must be print 2 but will print 1
+        // to solve this problem we will use singleton 
     }
 }
