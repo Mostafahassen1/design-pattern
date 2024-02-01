238ce61062ec4db79c0886708c3a8afe0af78c5d
Mostafahassen1 <134046265+Mostafahassen1@users.noreply.github.com>
2024-02-01 08:14:39 +0200

    Create Main.java

---
+++ Singleton/After-Singleton/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Counter obj1 = Counter.GetInstance() ; // create the new object
+        Counter obj2 = Counter.GetInstance(); // refer to the same index(obj1) in memory 
+
+        System.out.println( obj1.addCount() ) ; // in this case the obj1 print 1
+        System.out.println(obj2.addCount() ) ;  // and the obj2   print 2
+           // because obj1 , obj2 refer to the same index in the memory
+
+    }
+}
