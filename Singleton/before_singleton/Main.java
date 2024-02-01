d77468140ac1ff8510e6bfab784f31553a9a6a18
Mostafahassen1 <134046265+Mostafahassen1@users.noreply.github.com>
2024-02-01 07:35:47 +0200

    Create Main.java

---
+++ Singleton/before_singleton/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Counter obj1 = new Counter();
+        Counter obj2 = new Counter() ;
+
+        obj1.addCount();
+        obj2.addCount();
+    }
+}
