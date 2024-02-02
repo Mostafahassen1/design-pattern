a4549302cd1d98aca9db2d151a86f9875c319222
Mostafahassen1 <134046265+Mostafahassen1@users.noreply.github.com>
2024-02-02 05:29:55 +0200

    implement the interface product

--- factory-Method/1-before-Factory/SilverProduct.java
+++ factory-Method/1-before-Factory/SilverProduct.java
@@ -1,5 +1,5 @@
 public class SilverProduct implements ICreditCardProduct {
     public void showDetailes() {
-        System.out.println(" This is a Silver visa .....") ;
+        System.out.println(" This is a Silver visa ......") ;
     }
 }
