b06e22013c76301855df2f7e6545981fee22bc8d
Mostafahassen1 <134046265+Mostafahassen1@users.noreply.github.com>
2024-02-02 05:22:48 +0200

    Create Main.java

---
+++ factory-Method/1-before-Factory/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+     double salary = 10000 ;
+        ICreditCardProduct  product1 = CreditCardFactor.getCreditCardFactor(salary) ;
+        product1.showDetailes();
+
+
+
+    }
+}
